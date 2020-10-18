084a595a59b9654f6da0bf4c9a6d94e3054f9b32
Dan Mikheenko <miheenko-denis@mail.ru>
2020-10-18 17:56:29 +0300

    Added WPF project

---
+++ TagGameWpf/Infrastructure/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagGameWpf.Infrastructure
+{
+    interface ICommand
+    {
+        event EventHandler CanExecuteChanged;
+        void Execute(object parameter);
+        bool CanExecute(object parameter);
+    }
+}
---
+++ TagGameWpf/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TagGameLib;
+using TagGameWpf.Annotations;
+
+namespace TagGameWpf
+{
+    public class MainWindowViewModel:INotifyPropertyChanged
+    {
+        private Game _game;
+        private ObservableCollection<Tag> _tags;
+
+        public MainWindowViewModel()
+        {
+            _game = new Game();
+            _game.Start(4);
+            _tags = new ObservableCollection<Tag>();
+            
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
